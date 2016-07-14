d09e3800587f242c5a4da34a2ad5f7444b8d7af5
ArdaIcmez <aicmez@gmail.com>
2016-07-14 17:07:05 +0300

    fixed index problem

--- classifiers/trainer.py
+++ classifiers/trainer.py
@@ -10,8 +10,8 @@ def trainSVM(featArr, clusArr, labArr) :
         y = []
         x = []
         for j in i:
-            y.append(label[j-1])
-            x.append(featArr[j-1].tolist())
+            y.append(label[j])
+            x.append(featArr[j].tolist())
 
         print y, x
         prob  = svm_problem(y, x)
@@ -23,8 +23,8 @@ def trainSVM(featArr, clusArr, labArr) :
 
 def main():
 
-    cl1 = np.array([1, 2, 3,5,7])
-    cl2 = np.array([4,6,8])
+    cl1 = np.array([0, 1, 2,4,6])
+    cl2 = np.array([3,5,7])
     clusters = [cl1,cl2]
     features = np.array([[1, 2], [3, 9], [3, 9], [3, 9], [3, 9], [3, 9], [3, 9], [3, 9]])
     labels = np.array([1,3,2,3,2,1,2,2])
