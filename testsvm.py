0f93080d7605a011a592482a9ae9fad7b826b270
ahmetbaglan <ahmet.baglan1@gmail.com>
2016-07-14 16:36:55 +0300

    ilkTest

--- testsvm.py
+++ testsvm.py
@@ -1,30 +1,36 @@
 from svmutil import *
+import numpy as np
 
 def trainSVM(featArr, clusArr, labArr) :
     
     label = labArr.tolist()
     order = 0
-    for i in len(clusArr):
-        
+    for i in clusArr:
         #Find corresponding labels, create 'y' list
         y = []
         x = []
         for j in i:
-            y.append(label[j])
-            x.append(featArr[j])
-        
-        print y
-        print x
-        """
-        prob  = svm_problem(y, x, isKernel=True)
+            y.append(label[j-1])
+            x.append(featArr[j-1].tolist())
+
+        print y, x
+        prob  = svm_problem(y, x)
         param = svm_parameter('-t 2 -c 4')  # Gamma missing
         
         m = svm_train(prob, param)
         svm_save_model('clus' + `order` + '.model', m)
         order+=1
-        """
+
 def main():
-    pass
+
+    cl1 = np.array([1, 2, 3,5,7])
+    cl2 = np.array([4,6,8])
+    clusters = [cl1,cl2]
+    features = np.array([[1, 2], [3, 9], [3, 9], [3, 9], [3, 9], [3, 9], [3, 9], [3, 9]])
+    labels = np.array([1,3,2,3,2,1,2,2])
+
+    trainSVM(features, clusters,labels)
+
     
 if __name__ == '__main__':
     main()
