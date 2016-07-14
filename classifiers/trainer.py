5a2a7c9aa5adfe4fea86c5dfdc8a1052aaae3b7f
ArdaIcmez <aicmez@gmail.com>
2016-07-14 17:52:44 +0300

    minor changes

--- classifiers/trainer.py
+++ classifiers/trainer.py
@@ -39,7 +39,7 @@ def main():
     cl1 = np.array([0, 1, 2,4,6])
     cl2 = np.array([3,5,7])
     clusters = [cl1,cl2]
-    features = np.array([[1, 2], [3, 9], [3, 9], [3, 9], [3, 9], [3, 9], [3, 9], [3, 9]])
+    features = np.array([[1, 2], [2, 4], [5, 3], [1, 1], [3, 9], [2, 5], [8, 3], [3, 2]])
     labels = np.array([1,3,2,3,2,1,2,2])
 
     trainSVM(features, clusters,labels)
