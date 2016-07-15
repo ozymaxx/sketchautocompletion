2e1c812c452f9228c389907f496b263249bb7097
ArdaIcmez <aicmez@gmail.com>
2016-07-15 10:34:48 +0300

    Method to form the constraint matrix

---
+++ clusterer/getConstraints.py
@@ -0,0 +1,29 @@
+#
+# Author: TugRulz
+#
+#
+# inputs: size:total number of full and partial sketches, isFull: full or partial ? matrix , classId: class of sketch? matrix
+# output: constraint matrix (must or cannot link)
+import numpy
+def getConstraints(size, isFull, classId):
+    FULL = 1
+    MUST_LINK = 1
+    CANNOT_LINK = -1
+    DIAGONAL = 0
+    cons = numpy.zeros((size, size))
+    for index, el in enumerate(cons):
+        if (isFull[index] == FULL):
+            for index2, el2 in enumerate(el):
+                if (index == index2 ):
+                    cons[index][index2] = DIAGONAL
+                else:
+                    if (isFull[index2] == FULL):
+                        if(classId[index] == classId[index2]):
+                            cons[index][index2] = MUST_LINK
+                        else:
+                            cons[index][index2] = CANNOT_LINK
+    return cons
+classId = [1 , 3 , 2 , 3 , 1 , 5 , 2]
+isFull = [1 , 1 , 0 , 0 , 1 , 0 , 1]
+test = getConstraints(7, isFull, classId)
+print (test)
