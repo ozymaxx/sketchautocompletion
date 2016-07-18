c56b1473f51db07221be0b0648c729ea527de794
ahmetbaglan <ahmet.baglan1@gmail.com>
2016-07-18 10:08:25 +0300

    visualise by class is done

--- clusterer/ckmeans.py
+++ clusterer/ckmeans.py
@@ -8,6 +8,8 @@ from getConstraints import *
 import sys
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import matplotlib.markers as mark
+
 
 
 class CKMeans:
@@ -150,26 +152,29 @@ def visualiseBeforeClustering(out,features):
     plt.figure()
     plt.grid(True)
 
-def visualiseAfterClustering(out,features):
+def visualiseAfterClustering(out, features, classId):
     colorList = cm.rainbow(np.linspace(0, 1, len(out[0])))
     index = 0
+    numClass = len(set(classId))
     for cluster in out[0]:
         index+=1
         color = colorList[index-1]
-            
+
         for i in cluster.astype(int):
               x = features.tolist()[i]
               scale = 80
-
+              marker = classId[i]
+              plt
               plt.scatter(x[0], x[1], c=color, s=scale, label=color,
-                    alpha=0.5, edgecolors='black')
+                    alpha=0.5, edgecolors='black', marker= mark.MarkerStyle.filled_markers[marker])
 
     plt.grid(True)
     
     
 def main():
 
-    classId = [1 , 3 , 2 , 3 , 1 , 5 , 2]
+
+    classId = [1 , 3 , 2 , 3 , 1 , 4 , 2]
     isFull = [1 , 1 , 0 , 0 , 1 , 0 , 1]
     test = getConstraints(7, isFull, classId);
     features = np.array([[3,6,5,1,3,2,8],[2,3,3,1,9,5,3]])
@@ -177,7 +182,7 @@ def main():
     output = kmeans.getCKMeans()
     print output[0]
     print output[1]
-    visualiseAfterClustering(output,np.transpose(features))
+    visualiseAfterClustering(output,np.transpose(features),classId)
     plt.show()
 
 if __name__ == '__main__':
