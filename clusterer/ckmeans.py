c0abee951acc2f42445b4dff331cf238d8da8d34
ahmetbaglan <ahmet.baglan1@gmail.com>
2016-07-18 10:47:38 +0300

    Head

--- clusterer/ckmeans.py
+++ clusterer/ckmeans.py
@@ -8,6 +8,7 @@ from getConstraints import *
 import sys
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import matplotlib.markers as mark
 
 
 class CKMeans:
@@ -160,21 +161,35 @@ def visualiseBeforeClustering2(features):
     plt.figure()
     plt.grid(True)
     
-def visualiseAfterClustering(out,features):
+def visualiseAfterClustering(out,features, classId):
+    def getMarkerList():
+        numClass = len(set(classId))
+        marker_list = list(mark.MarkerStyle.filled_markers)
+        markIndex = 4
+        while numClass >= len(marker_list):
+            marker_list.append((markIndex,1))
+            markIndex+=1
+
+        return marker_list
+
+
     colorList = cm.rainbow(np.linspace(0, 1, len(out[0])))
     index = 0
+    marker_list = getMarkerList()
     for cluster in out[0]:
         index+=1
         color = colorList[index-1]
-            
+
         for i in cluster.astype(int):
               x = features.tolist()[i]
               scale = 80
-
+              marker = classId[i]
+              plt
               plt.scatter(x[0], x[1], c=color, s=scale, label=color,
-                    alpha=0.5, edgecolors='black')
+                    alpha=0.5, edgecolors='black', marker= marker_list[marker])
 
     plt.grid(True)
+
     
     
 def main():
@@ -183,17 +198,17 @@ def main():
     isFull = [1 , 1 , 0 , 0 , 1 , 0 , 1]
     test = getConstraints(7, isFull, classId);
     print test
-<<<<<<< HEAD
 
-=======
->>>>>>> 867c2d3b0404bb5207163874a8f51698ced5adc7
+
+
+
     features = np.array([[3,6,5,1,3,2,8],[2,3,3,1,9,5,3]])
     visualiseBeforeClustering2(np.transpose(features))
     kmeans = CKMeans(test,features,4)
     output = kmeans.getCKMeans()
     print output[0]
     print output[1]
-    visualiseAfterClustering(output,np.transpose(features))
+    visualiseAfterClustering(output,np.transpose(features), classId)
     plt.show()
 
 if __name__ == '__main__':
