ea11adb83eec59e34a59df22571ae881f501beea
ArdaIcmez <aicmez@gmail.com>
2016-07-19 16:13:47 +0300

    Feature extraction

--- predict/feature.py
+++ predict/feature.py
@@ -9,24 +9,36 @@ import os
 sys.path.append("../../sketchfe/sketchfe")
 
 from sketchfe import shapecreator
+import sketchfe.FeatureExtractor.IDMFeatureExtractor
 
-def featureExtract(filename):	
-	with open(filename,'rb') as f:
-		extractType = None
-		extension = os.path.splitext(filename)[1]
-		
-		if extension == ".json":
-			extractType = "json"
+def featureExtract(filename):
+	loadedSketch = None
+	
+	try:
+		with open(filename,'rb') as f:
+			extractType = None
+			extension = os.path.splitext(filename)[1]
 			
-		elif extension == ".xml":
-			extractType = "xml"
-			
-		elif extension == ".txt":
-			extractType = "school"
-			
-		else:
-			print "Wrong file type"
-			exit
-			
-		filecontent = f.read()
-		loadedSketch = shapecreator.buildSketch(filecontent,extractType)
+			if extension == ".json":
+				extractType = "json"
+				
+			elif extension == ".xml":
+				extractType = "xml"
+				
+			elif extension == ".txt":
+				extractType = "school"
+				
+			else:
+				print "Wrong file type"
+				exit
+				
+			filecontent = f.read()
+			loadedSketch = shapecreator.buildSketch(filecontent,extractType)
+	except:
+		print "Error loading the sketch"
+		exit
+	
+	featextractor = IDMFeatureExtractor()
+	features = featextractor.extract(loadedSketch)
+	
+	
