9c76803c68e5ffa67b95242fd7eb36431411b7d9
ArdaIcmez <aicmez@gmail.com>
2016-07-19 10:35:00 +0300

    Implementation of cluster + classifier

--- classifiers/trainer.py
+++ classifiers/trainer.py
@@ -4,23 +4,30 @@ Ahmet BAGLAN - Arda ICMEZ
 14.07.2016
 """
 from svmutil import *
+from numpy.random import rand
 import numpy as np
 import matplotlib.pyplot as plt
+from ckmeans import *
+from getConstraints import *
 
 def trainSVM(featArr, clusArr, labArr) :
 
-    label = labArr.tolist()
+    #label = labArr.tolist()
+    label = labArr
     order = 0
     for i in clusArr:
         #Find corresponding labels, create 'y' list
         y = []
         x = []
+        
         for j in i:
+            j = int(j)
             y.append(label[j])
             x.append(featArr[j].tolist())
 
+        print y, x
         prob  = svm_problem(y, x)
-        param = svm_parameter('-t 2 -c 4 -g 0.125 -b 1')  # Gamma missing
+        param = svm_parameter('-t 2 -c 4')  # Gamma missing
         
         m = svm_train(prob, param)
         svm_save_model('clus' + `order` + '.model', m)
@@ -32,57 +39,73 @@ def computeProb(numIns, clusArr):
         prob.append(float(len(c))/numIns)
     return prob
 
-def plotClassCluster(features,labels,clusArr):
-    x = features.tolist()
-    y = labels.tolist()
-
-    scale = 80
-    color = 'purple'
-    numClus = len(clusArr)
-    marker = 0
-    for j in range(0,len(y)):
-        if(y[j] == 1):
-            color = 'red'
-        elif(y[j] == 2):
-            color = 'blue'
-        else:
-            color = 'green'
-        for k in range(numClus):
-            if( j in clusArr[k]):
-                marker = k
-
-        plt.scatter(x[j][0], x[j][1], c=color, s=scale, label=color,
-            alpha=0.5, edgecolors='black', marker = (numClus +1,marker+1))
-
-    plt.grid(True)
-    plt.show()
-
-
-
 def main():
-########## Test Case  ########################
-    cl1 = np.array([0, 1, 2,4,6])
-    cl2 = np.array([3,5,7,9])
-    cl3 = np.array([8,10])
-    clusters = [cl1,cl2,cl3]
-    features = np.array([[1, 2], [2, 4], [5, 3], [1, 1], [3, 9], [2, 5], [8, 3], [3, 2],[2,2],[3,5],[4,4]])
-    labels = np.array([1,3,2,3,2,1,2,2,1,2,3])
+########## Test Case  #######################
 
-    trainSVM(features, clusters,labels)
+    
 
-########## Calculate probabilities  ########################
+    NUMPOINTS = 200;
+    NUMCLASS = 12;
+    POINTSPERCLASS = NUMPOINTS/NUMCLASS
 
+    xmin = 0;
+    xmax = 100;
+    ymin = 0;
+    ymax = 100;
+
+    features = np.array([np.zeros(NUMPOINTS), np.zeros(NUMPOINTS)])
+    centers = np.array([np.zeros(NUMCLASS), np.zeros(NUMCLASS)])
+    isFull = [np.random.randint(0, 2) for r in xrange(NUMPOINTS)]
+
+    classId = list()
+    index = 0
+##################################### PREPARE FEATURES ###################################
+    for i in range(0, NUMCLASS): 
+        classId.extend([i]*POINTSPERCLASS)
+        centerx = int(np.random.random()*xmax - xmin)
+        centery = int(np.random.random()*ymax - ymin)
+        centers[0][i] = centerx
+        centers[1][i] = centery
+
+        for j in range(0, POINTSPERCLASS):
+            datax = int(np.random.normal(loc = centerx, scale = 3))
+            datay = int(np.random.normal(loc = centery, scale = 3))
+
+            features[0][index] = datax
+            features[1][index] = datay
+            index += 1
+
+    # add the remaning points, from integer division
+    remainingPoints = NUMPOINTS - POINTSPERCLASS*NUMCLASS
+    if (remainingPoints):
+        # select the center randomly
+        randc = np.random.randint(0, max(classId))
+        classId.extend([randc]*remainingPoints)
+        for i in range (NUMPOINTS - POINTSPERCLASS*NUMCLASS):
+            datax = int(np.random.normal(loc = centers[0][randc], scale = 3))
+            datay = int(np.random.normal(loc = centers[1][randc], scale = 3))
+            features[0][index] = datax
+            features[1][index] = datay
+            index += 1
+#######################################################################################
+
+    test = getConstraints(NUMPOINTS, isFull, classId)
+    
+    k = 13
+    kmeans = CKMeans(test,features,k)
+    output = kmeans.getCKMeans()
+    
+    """
     probabilities = computeProb(len(labels),clusters)
 
     print probabilities
-
-########## Do plotting  ########################
-
-    plotClassCluster(features,labels,clusters)
-
+    """
 #################################################
 
+    trainSVM(np.transpose(features), output[0], classId)
+   
 
+    
 if __name__ == '__main__':
     main()
     #profile.run('print main(); print')
