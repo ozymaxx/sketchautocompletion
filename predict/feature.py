e9183c9c304b10cfb22ad177e895fa1f389a0981
ArdaIcmez <aicmez@gmail.com>
2016-07-19 15:55:45 +0300

    Feature extractor implementation start

---
+++ predict/feature.py
@@ -0,0 +1,16 @@
+"""
+Feature extraction
+Arda ICMEZ
+14.07.2016
+"""
+
+import sys
+sys.path.append("../../sketchfe/sketchfe")
+
+from sketchfe import FeatureExtractor
+idm = FeatureExtractor.IDMFeatureExtractor()
+idm.extractimage_test()
+
+def featureExtract():
+	pass
+
