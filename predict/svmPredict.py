3909e97c21412442a6e993018dc3d9aa185ede1b
ArdaIcmez <aicmez@gmail.com>
2016-07-20 11:40:06 +0300

    predict

--- predict/svmPredict.py
+++ predict/svmPredict.py
@@ -4,4 +4,4 @@ def svmProb(model,features):
     m = svm_load_model(model)
     y =  [0] * len(features[0])
     p_label, p_acc, p_val = svm_predict(y, features, m, '-b 1')
-    return p_val
+    return (p_label, p_val)
