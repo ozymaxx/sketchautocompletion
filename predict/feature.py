2af692ebf4539ab80b2fea0ba573b397354e36ed
Semih Gunel <gunelsemih@gmail.com>
2016-07-22 12:06:24 +0300

    Test with two classes database set

--- predict/feature.py
+++ predict/feature.py
@@ -7,12 +7,21 @@ Arda ICMEZ
 import sys
 import os
 sys.path.append("../../sketchfe/sketchfe")
-import sys
 sys.path.append('../predict/')
+sys.path.append('../clusterer/')
+sys.path.append('../classifiers/')
+sys.path.append("../../libsvm-3.21/python/")
 from feature import *
 
 from FeatureExtractor import *
 from shapecreator import *
+
+import numpy as np
+from svmutil import *
+import matplotlib.pyplot as plt
+from ckmeans import *
+from getConstraints import *
+import visualise
 
 def featureExtract(filename):
 	loadedSketch = None
@@ -44,5 +53,42 @@ def featureExtract(filename):
 	features = featextractor.extract(loadedSketch)
 	return features
 
-feature = featureExtract('../json/castle/castle_3.json')
-print feature
+def main():
+	features = list()
+	classId = list()
+	for i in range(1,79):
+		filename = '../json/airplane/airplane_' + str(i) + ".json"
+		print filename
+		feature = featureExtract(filename)
+		features.append(np.array(feature))
+	classId.extend([0]*len(features))
+
+	for i in range(1,79):
+		filename = '../json/castle/castle_' + str(i) + ".json"
+		print filename
+		feature = featureExtract(filename)
+		features.append(np.array(feature))
+	classId.extend([2]*len(features))
+
+	NUMPOINTS = len(features)
+	isFull = [0]*NUMPOINTS
+
+	test = getConstraints(NUMPOINTS, isFull, classId)
+	kmeans = CKMeans(test,np.transpose(features),2)
+	output = kmeans.getCKMeans()
+	a = 5
+
+    # print computeProb(output)
+#################################################
+    # for all clusters
+	'''
+    clustersToBeTrained, toBeTrainedId = getHeterogenous(output,classId)
+    allSV = trainSVM(np.transpose(features), clustersToBeTrained, classId)
+
+    visualise.visualiseAfterClustering(allSV,output,np.transpose(features), classId, isFull, centers, "cluster number not defined")
+    plt.show()
+	'''
+if __name__ == '__main__':
+    main()
+    print "fin"
+    #profile.run('print main(); print')
