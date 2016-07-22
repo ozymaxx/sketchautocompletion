b7e508a2caad703c1d63559567c188ca4e489d42
Semih Gunel <gunelsemih@gmail.com>
2016-07-22 10:51:33 +0300

    Corrected JSON files Corrected feature extraction

--- predict/feature.py
+++ predict/feature.py
@@ -7,38 +7,42 @@ Arda ICMEZ
 import sys
 import os
 sys.path.append("../../sketchfe/sketchfe")
+import sys
+sys.path.append('../predict/')
+from feature import *
 
-from sketchfe import shapecreator
-import sketchfe.FeatureExtractor.IDMFeatureExtractor
+from FeatureExtractor import *
+from shapecreator import *
 
 def featureExtract(filename):
 	loadedSketch = None
-	
 	try:
 		with open(filename,'rb') as f:
 			extractType = None
 			extension = os.path.splitext(filename)[1]
-			
-			if extension == ".json":
-				extractType = "json"
-				
+
+			if extension == '.json':
+				extractType = 'json'
+
 			elif extension == ".xml":
 				extractType = "xml"
-				
+
 			elif extension == ".txt":
 				extractType = "school"
-				
+
 			else:
 				print "Wrong file type"
 				exit
-				
+
 			filecontent = f.read()
-			loadedSketch = shapecreator.buildSketch(filecontent,extractType)
+			loadedSketch = shapecreator.buildSketch(extractType, filecontent)
 	except:
 		print "Error loading the sketch"
 		exit
-	
+
 	featextractor = IDMFeatureExtractor()
 	features = featextractor.extract(loadedSketch)
-	
-	print "lol"
+	return features
+
+feature = featureExtract('../json/castle/castle_3.json')
+print feature
