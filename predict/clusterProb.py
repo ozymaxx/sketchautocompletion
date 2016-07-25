f3d9fe126473e09d19c1353f76a9be23ea1c95b1
semihgunel <gunelsemih@gmail.com>
2016-07-25 12:14:57 +0300

    Update clusterProb.py

--- predict/clusterProb.py
+++ predict/clusterProb.py
@@ -27,11 +27,8 @@ def clusterProb(centers,instance,normalProb):
     # Returns P(Ck|x)
     probTup = []
     for i in range(len(centers)):
-        c_x = centers[i][0]
-        c_y = centers[i][1]
-        a = (instance[0], instance[1])
-        b = (c_x, c_y)
-        dist = computeDistance(a, b)
+
+        dist = computeDistance(instance, centers[i])
         probTup.append(math.exp(-1*abs(dist))*normalProb[i])
     return probTup
 
@@ -56,11 +53,11 @@ def calculateProb(instance, kmeansoutput, priorClusterProb, classId):#It would i
         if clstrid in homoClstrId:
             # if homogeneous cluster is empty, then do not
             # process it and continue
-            if not any(kmeansoutput[0][clstrid]):
+            if len(kmeansoutput[0][clstrid]) == 0:
                 continue
             # if homogeneous then only a single class which is the first
             # feature points class
-            classesInCluster =  [classId[kmeansoutput[0][clstrid][0]]]
+            classesInCluster = [classId[kmeansoutput[0][clstrid][0]]]
         elif clstrid in heteClstrId:
             modelName = "clus"+ ` heteClstrId.index(clstrid) ` +".model"
             m = svm_load_model('../classifiers/' + modelName)
@@ -72,58 +69,12 @@ def calculateProb(instance, kmeansoutput, priorClusterProb, classId):#It would i
 
     return outDict
 
-def calculateProb2(instance, kmeansoutput, priorClusterProb, classId):#It would increase performance to get list of instance!!!!!!!!!!!!!!!!
-    #features : feature array
-    #output : list [ List of Cluster nparray, List of Cluster Center nparray]
-    #probability : P(Ck)
-    # Returns P(Si|x)
-
-    # dict of probabilities of given instance belonging to every possible class
-    # initially zero
-    outDict = dict.fromkeys([i for i in set(classId)], 0.0)
-
-
-    homoClstrFeatureId, homoClstrId = getHomogenous(kmeansoutput, classId)
-    heteClstrFeatureId, heteClstrId = getHeterogenous(kmeansoutput, classId)
-    clusterPrb  = clusterProb(kmeansoutput[1], instance, priorClusterProb)
-    # normalize cluster probability to add up to 1
-    clusterPrb = [x/sum(clusterPrb) for x in clusterPrb]
-    print sum(clusterPrb)
-
-    for i in range(len(heteClstrId)):
-        modelName = "clus"+ `i` +".model"
-        m = svm_load_model('../classifiers/' + modelName)
-        classesInCluster = m.get_labels()
-        labels, probs = svmProb(m, [instance.tolist()])
-        probabilityToBeInThatCluster = clusterPrb[heteClstrId[i]]
-
-        for c in range(len(classesInCluster)):
-            probabilityToBeInThatClass = probs[0][c]
-            outDict[int(classesInCluster[c])] += probabilityToBeInThatCluster * probabilityToBeInThatClass
-
-    print sum(outDict.values()), 'heto'
-
-    for id in homoClstrId:
-        clusterFeatures = kmeansoutput[0][id]
-        # check if not empty
-        if any(clusterFeatures):
-            # take the class of the first feature
-            clusterClass = classId[clusterFeatures[0]]
-            outDict[clusterClass] += clusterPrb[id]
-            #print clusterPrb[clusterClass], clusterPrb
-
-    print [clusterPrb[x] for x in homoClstrId]
-    print sum(outDict.values())
-    return outDict
-
-
 def main():
 ########## Test Case  #######################
     # delete all ".model" files before creating them again
     files = os.listdir('../classifiers/')
     for file in files:
         extension = os.path.splitext(file)[1]
-
         if extension == '.model':
             os.remove('../classifiers/'+file)
 
