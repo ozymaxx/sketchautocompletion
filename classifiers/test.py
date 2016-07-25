f0002ee60249d86241d5fad81664994e864735ea
Arda Icmez <aicmez@gmail.com>
2016-07-25 17:03:06 +0300

    Delete test.py

--- classifiers/test.py
+++
@@ -1,9 +0,0 @@
-from svmutil import *
-
-m = svm_load_model('clus0.model')
-y = [1,2]
-p_label, p_acc, p_val = svm_predict(y,[[3,9],[2,2]], m, '-b 1')
-
-print p_label,"label"
-print "accuracy ",p_acc
-print "val ", p_val
