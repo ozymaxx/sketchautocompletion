3233f8c11c91ce778eaa7ae51acc028af6fada91
ArdaIcmez <aicmez@gmail.com>
2016-07-25 16:04:04 +0300

    Trainer class completed

--- classifiers/trainer.py
+++ classifiers/trainer.py
@@ -7,101 +7,75 @@ import sys
 sys.path.append("../../libsvm-3.21/python/")
 
 from svmutil import *
-from numpy.random import rand
-import numpy as np
-import matplotlib.pyplot as plt
-from ckmeans import *
-from getConstraints import *
-from testCases import *
-import visualise
+import copy
 
-def trainSVM(featArr, clusArr, labArr):
-
-    #label = labArr.tolist()
-    label = labArr
-    order = 0
-    allModels = list()
-    for i in clusArr:
-        #Find corresponding labels, create 'y' list
-        y = []
-        x = []
-        
-        for j in i:
-            j = int(j)
-            y.append(label[j])
-            x.append(featArr[j].tolist())
-
-        print y, x
-        prob  = svm_problem(y, x)
-        param = svm_parameter('-s 0 -t 2 -g 0.125 -c 8 -b 1 -q')
-        
-        m = svm_train(prob, param,)
-        allModels.append(m.get_SV())
-        svm_save_model('../classifiers/clus' + `order` + '.model', m)
-        order+=1
-
-    return allModels
-
-def computeProb(out):
-    prob = []
-    total =  0
-
-    for c in out[0]:
-        total += len(c)
-
-    for cluster in out[0]:
-        prob.append(len(cluster)/float(total))
-    return prob
-
-def getHeterogenous(output,classId):
-    clustersToBeTrained = list()
-    clustersIdsToBeTrained = list()
-    for clusterId in range(len(output[0])):
-        # if class id of any that in cluster of clusterId is any different than the first one
-        if any(x for x in range(len(output[0][clusterId])) if classId[int(output[0][clusterId][0])] != classId[int(output[0][clusterId][x])]):
-            clustersToBeTrained.append(output[0][clusterId])
-            clustersIdsToBeTrained.append(clusterId)
-    return clustersToBeTrained, clustersIdsToBeTrained
-
-def getHomogenous(output, classId):
-    homoClass = list()
-    homoIdClus = list()
-    for clusterId in range(len(output[0])):
-        # if class id of any that in cluster of clusterId is any different than the first one
-        if not any(x for x in range(len(output[0][clusterId])) if classId[int(output[0][clusterId][0])] != classId[int(output[0][clusterId][x])]):
-            homoClass.append(output[0][clusterId])
-            homoIdClus.append(clusterId)
-    return homoClass, homoIdClus
+class Trainer:
     
-
-def main():
-########## Test Case  #######################
-    NUMPOINTS = 200;
-    NUMCLASS = 12;
-    POINTSPERCLASS = NUMPOINTS/NUMCLASS
-    k = 13
-###################################################################################
-
-    (features, isFull, classId, centers) = getFeatures(NUMPOINTS,NUMCLASS)
-
-    test = getConstraints(NUMPOINTS, isFull, classId)
-    kmeans = CKMeans(test,features,k)
-    output = kmeans.getCKMeans()
-
-
-    # print computeProb(output)
-#################################################
-    # for all clusters
-
-    clustersToBeTrained, toBeTrainedId = getHeterogenous(output,classId)
-    allSV = trainSVM(np.transpose(features), clustersToBeTrained, classId)
-
-    visualise.visualiseAfterClustering(allSV,output,np.transpose(features), classId, isFull, centers, "cluster number not defined")
-    plt.show()
-
-if __name__ == '__main__':
-    main()
-    #profile.run('print main(); print')
+    def __init__(self, featArr, labArr, output, classId):
+        # output[0] : list of clusters
+        # output[1] : list of cluster centers
+        self.featArr = featArr
+        self.labArr = labArr
+        self.output = output
+        self.classId = classId
+        
+    def trainSVM(self):
+        # Trains the support vector machine
+        label = copy.copy(self.labArr)
+        order = 0
+        allModels = list()
+        for i in self.output[0]:
+            y = []
+            x = []
+            for j in i:
+                j = int(j)
+                y.append(label[j])
+                x.append(self.featArr[j].tolist())
+    
+            print y, x
+            prob  = svm_problem(y, x)
+            param = svm_parameter('-s 0 -t 2 -g 0.125 -c 8 -b 1 -q')
+            
+            m = svm_train(prob, param,)
+            allModels.append(m.get_SV())
+            svm_save_model('../classifiers/clus' + `order` + '.model', m)
+            order+=1
+    
+        return allModels
+    
+    def computeProb(self):
+        # Returns probability list of being in a cluster
+        prob = []
+        total =  0
+    
+        for c in self.output[0]:
+            total += len(c)
+    
+        for cluster in self.output[0]:
+            prob.append(len(cluster)/float(total))
+        return prob
+    
+    def getHeterogenous(self):
+        # Gets clusters which are heterogenous
+        clustersToBeTrained = list()
+        clustersIdsToBeTrained = list()
+        for clusterId in range(len(self.output[0])):
+            # if class id of any that in cluster of clusterId is any different than the first one
+            if any(x for x in range(len(self.output[0][clusterId])) if self.classId[int(self.output[0][clusterId][0])] != self.classId[int(self.output[0][clusterId][x])]):
+                clustersToBeTrained.append(self.output[0][clusterId])
+                clustersIdsToBeTrained.append(clusterId)
+        return clustersToBeTrained, clustersIdsToBeTrained
+    
+    def getHomogenous(self):
+        # Gets clusters which are homogenous
+        homoClass = list()
+        homoIdClus = list()
+        for clusterId in range(len(self.output[0])):
+            # if class id of any that in cluster of clusterId is any different than the first one
+            if not any(x for x in range(len(self.output[0][clusterId])) if self.classId[int(self.output[0][clusterId][0])] != self.classId[int(self.output[0][clusterId][x])]):
+                homoClass.append(self.output[0][clusterId])
+                homoIdClus.append(clusterId)
+        return homoClass, homoIdClus
 
 
 
