139ef446661c5dac09a2b5005923ffda3164e2d7
ahmetbaglan <ahmet.baglan1@gmail.com>
2016-07-26 16:41:36 +0300

    Main class improved

--- predict/Main.py
+++ predict/Main.py
@@ -1,33 +1,35 @@
 
 import sys
-import os
+
 sys.path.append("../../sketchfe/sketchfe")
 sys.path.append('../predict/')
 sys.path.append('../clusterer/')
 sys.path.append('../classifiers/')
 sys.path.append("../../libsvm-3.21/python/")
 
-print sys.path
 from trainer import *
 from ckmeans import *
 from getConstraints import *
-from Predictor import *
 from FeatureExtractor import *
 from shapecreator import *
-import numpy as np
-from svmutil import *
 from feature import *
 
 class Main:
+    """The main class to be called"""
     def __init__(self):
         self.features = list()
         self.classId = list()
         self.isFull = list()
         self.path = '../json/'
-
-    def doIt(self, NUMFULLSKETCHPERCLASS,NUMPARTIALSKETCHPERFULL,NUMCLASS):
+        self.trainer = None
+        self.kmeansoutput = None
+        self.priorClusterProb = None
+        self.d = {}
+    def trainIt(self, NUMFULLSKETCHPERCLASS, NUMPARTIALSKETCHPERFULL, NUMCLASS, k):
+        """P"""
         classIdCount= 0
         pathdir = ['airplane', 'alarm-clock', 'angel', 'ant', 'apple']
+
         for folder in pathdir:
             # iterate over folders in pathdir
             if classIdCount == NUMCLASS:
@@ -46,6 +48,7 @@ class Main:
                 feature = featureExtract(fullsketchpath)
                 self.features.append(np.array(feature))
                 self.classId.append(classIdCount)
+                self.d[classIdCount] = folder
                 self.isFull.append(1)
                 partialsketchcount = 1
                 while os.path.isfile(self.path + folder + '/' + folder + '_' + str(sketchcounter) + "_" + str(partialsketchcount) + '.json'):
@@ -67,27 +70,43 @@ class Main:
 
         NUMPOINTS = len(self.features)
         test = getConstraints(NUMPOINTS, self.isFull, self.classId)
-        ckmeans = CKMeans(test, np.transpose(self.features), NUMCLASS)
-        kmeansoutput = ckmeans.getCKMeans()
+        ckmeans = CKMeans(test, np.transpose(self.features), k)
+        self.kmeansoutput = ckmeans.getCKMeans()
 
         # find heterogenous clusters and train svm
-        trainer = Trainer(kmeansoutput, self.classId, self.features) ### FEATURES : TRANSPOSE?
-        heteClstrFeatureId, heteClstrId = trainer.getHeterogenous()
-        trainer.trainSVM(heteClstrFeatureId)
-        # find the probability of given feature to belong any of the classes
-        priorClusterProb = trainer.computeProb()
-        predictor = Predictor(kmeansoutput,self.classId)
-        outDict = predictor.calculateProb(featureExtract('../json/airplane/airplane_1.json'), priorClusterProb)
+        self.trainer = Trainer(self.kmeansoutput, self.classId, self.features) ### FEATURES : TRANSPOSE?
+        heteClstrFeatureId, heteClstrId = self.trainer.getHeterogenous()
+        self.trainer.trainSVM(heteClstrFeatureId)
+        self.priorClusterProb = self.trainer.computeProb()
 
-        print outDict
+
+    def predictItInTheSet(self, instance):
+        # find the probability of given feature to belong any of the classes
+        loc = '../json/'+instance+'.json'
+        predictor = Predictor(self.kmeansoutput,self.classId)
+        outDict = predictor.calculateProb(featureExtract(loc), self.priorClusterProb)
+        d1= {}
+        for i in outDict:
+            d1[self.d[i]] = outDict[i]
+        return d1
+
+    def predictItUsingFeatures(self,feature):
+
+        predictor = Predictor(self.kmeansoutput,self.classId)
+        outDict = predictor.calculateProb(feature, self.priorClusterProb)
+        return outDict
 
 
 
 def main():
 
     m = Main()
-    m.doIt(2,12,2)
+    m.trainIt(5, 12, 4,8)
 
+    toBeChecked =[ 'airplane/airplane_80','airplane/airplane_78','airplane/airplane_77','ant/ant_15']
+
+    for i in toBeChecked:
+        print m.predictItInTheSet(i)
 if __name__ == '__main__':
     main()
     #profile.run('print main(); print')
