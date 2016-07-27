891a81bb2f7793b1fcb0bc6930757169ae5e09cd
tugrulz <tugrulcanelmas95@gmail.com>
2016-07-27 16:37:59 +0300

    run.py overwrite

--- run.py
+++ run.py
@@ -7,3 +7,12 @@ def run(jason):
     ## REAL DEAL
     # n = Main()
     # n.predict(jason)
+
+#
+# import sys
+# sys.path.append("./predict/")
+# from Main import *
+#
+# def run(jason):
+#     m = M()
+#     m.trainIt(2,10,10,2)
