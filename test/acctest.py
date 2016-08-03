05d7e7e5033445b34310b786e74a21586137ba86
Semih Gunel <gunelsemih@gmail.com>
2016-08-03 15:52:36 +0300

    calculate accuracy for different k, n and c values

--- test/acctest.py
+++ test/acctest.py
@@ -10,9 +10,26 @@ from extractor import *
 from featureutil import *
 import itertools
 import os
+import operator
+
+def trainIt(trainingName, trainingpath, numclass, numfull, numpartial, k, files):
+
+    constarr = getConstraints(size=len(features), isFull=isFull, classId=classId)
+    ckmeans = CKMeans(constarr, np.transpose(features), k)
+    kmeansoutput = ckmeans.getCKMeans()
+
+    # find heterogenous clusters and train svm
+    trainer = Trainer(kmeansoutput, classId, features)
+    heteClstrFeatureId, heteClstrId = trainer.getHeterogenous()
+    fio.saveTraining(names, classId, isFull, features, kmeansoutput,
+                     trainingpath, trainingName)
+    trainer.trainSVM(heteClstrFeatureId, trainingpath)
+    return kmeansoutput, classId
+
 def main():
+    '''
     ForceTrain = False
-    numclass, numfull, numpartial, numtest = 10, 10, 3, 5
+    numclass, numfull, numpartial = 10, 10, 3
     k = numclass
 
     trainingName = '%s__CFPK_%i_%i_%i_%i' % ('training', numclass, numfull, numpartial, k)
@@ -37,25 +54,28 @@ def main():
         whole_names, \
         folderList = extr.loadfolders(
                               numclass,
-                              numfull=numfull+numtest,
-                              numpartial= numpartial)
-        trainfeatures,\
-        trainisFull,\
-        trainclassId,\
-        trainnames,\
-        testfeatures,\
-        testisFull,\
-        testnames,\
-        testclassid = partitionfeatures_notin(
+                              numfull    = 80,
+                              numpartial = 80,
+                              folderList = folderList)
+        train_features,\
+        train_isFull,\
+        train_classId,\
+        train_names,\
+        test_features,\
+        test_isFull,\
+        test_names,\
+        test_classid = partitionfeatures_notin(
                                     whole_features,
                                     whole_isFull,
                                     whole_classId,
                                     whole_names,
-                                    numtest,
+                                    numtestfull,
+                                    numtestpartial,
                                     train_names,
                                     selectTestRandom=True)
-
     else: # else train
+
+
         whole_features,\
         whole_isFull,\
         whole_classId,\
@@ -78,7 +98,8 @@ def main():
                                     whole_isFull,
                                     whole_classId,
                                     whole_names,
-                                    numtest,
+                                    numtestfull,
+                                    numtestpartial,
                                     selectTestRandom=True)
 
         constarr = getConstraints(size=len(train_features), isFull=train_isFull, classId=train_classId)
@@ -88,36 +109,144 @@ def main():
         # find heterogenous clusters and train svm
         trainer = Trainer(kmeansoutput, train_classId, train_features)
         heteClstrFeatureId, heteClstrId = trainer.getHeterogenous()
-        fio.saveTraining(names, classId, isFull, features, kmeansoutput,
+        fio.saveTraining(train_names, train_classId, train_isFull, train_features, kmeansoutput,
                          trainingpath, trainingName)
         trainer.trainSVM(heteClstrFeatureId, trainingpath)
 
     predictor = Predictor(kmeansoutput, train_classId, trainingpath)
+    priorClusterProb = predictor.calculatePriorProb()
+
 
     '''
+    numclass, numfull, numpartial, xfold = 5, 5, 5, 5
 
-    Test and training data is ready
-    Predictor is ready
+    extr = Extractor('../data/')
+    whole_features, \
+    whole_isFull, \
+    whole_classId, \
+    whole_names, \
+    folderList = extr.loadfolders(
+                            numclass,
+                            numfull=numfull,
+                            numpartial=numpartial)
+
+
+    xfold_features, \
+    xfold_isFull, \
+    xfold_classId, \
+    xfold_names =  xfoldpartition(
+                        xfold,
+                        whole_features,
+                        whole_isFull,
+                        whole_classId,
+                        whole_names,
+                        folderList)
+
+
+    K = [xfold] # :O
+    N = range(2, 3)
+    C = range(30, 31)
+    accuracy = dict()
+    delay_rate = dict()
+
+    for k, n, c in itertools.izip(K, N, C):
+        accuracy[(k, n, c, True)] = 0
+        accuracy[(k, n, c, False)] = 0
+        delay_rate[(k, n, c, True)] = 0
+        delay_rate[(k, n, c, False)] = 0
+
+    for k in K:
+        for it in range(xfold):
+            test_features, test_isFull, test_classId, test_names = \
+                xfold_features[it], xfold_isFull[it], xfold_classId[it], xfold_names[it]
+
+            train_features, train_isFull, train_classId, train_names = [],[],[],[]
+            for trainIter in range(xfold):
+                if trainIter != it:
+                    train_features.extend(xfold_features[trainIter])
+                    train_isFull.extend(xfold_isFull[trainIter])
+                    train_classId.extend(xfold_classId[trainIter])
+                    train_names.extend(xfold_names[trainIter])
+
+            '''
+            Testing and training
+            data is ready
+            '''
+
+            '''
+            Training start
+            '''
+
+            ForceTrain = False
+            trainingName = '%s__CFPK_%i_%i_%i_%i_%i' % ('xfold_cv_' + str(it), numclass, numfull, numpartial, k, xfold)
+            print trainingName
+            trainingpath = '../data/training/' + trainingName
+
+            # if training data is already computed, import
+            fio = FileIO()
+            if os.path.exists(trainingpath) and not ForceTrain:
+                # can I assume consistency with classId and others ?
+                _, _, _, _, kmeansoutput, _ = fio.loadTraining(
+                    trainingpath + "/" + trainingName)
+            else:
+                constarr = getConstraints(size=len(train_features), isFull=train_isFull, classId=train_classId)
+                ckmeans = CKMeans(constarr, np.transpose(train_features), k)
+                kmeansoutput = ckmeans.getCKMeans()
+
+                # find heterogenous clusters and train svm
+                trainer = Trainer(kmeansoutput, train_classId, train_features)
+                heteClstrFeatureId, heteClstrId = trainer.getHeterogenous()
+                fio.saveTraining(train_names, train_classId, train_isFull, train_features, kmeansoutput,
+                                 trainingpath, trainingName)
+                trainer.trainSVM(heteClstrFeatureId, trainingpath)
+
+            predictor = Predictor(kmeansoutput, train_classId, trainingpath)
+            priorClusterProb = predictor.calculatePriorProb()
+
+            for test_index in range(len(test_features)):
+                Tfeature = test_features[test_index]
+                TtrueClass = test_classId[test_index]
+                classProb = predictor.calculatePosteriorProb(Tfeature, priorClusterProb)
+
+                SclassProb = sorted(classProb.items(), key=operator.itemgetter(1))
+                SclassProb = SclassProb[-n:]
+
+                summedprob = sum(tup[1] for tup in SclassProb)*100
+                summedclassId = [tup[0] for tup in SclassProb]
+
+                for n, c in itertools.izip(N, C):
+                    if summedprob < c:
+                        delay_rate[(k, n, c, test_isFull[test_index])] += 1
+                    else:
+                        if TtrueClass in summedclassId:
+                            accuracy[(k, n, c, test_isFull[test_index])] += 1
+
+            print trainingName + ' end'
+
+        for key in accuracy:
+            accuracy[key] = accuracy[key]*1.0/len(whole_features)*100
+
+        for key in delay_rate:
+            delay_rate[key] = delay_rate[key]*1.0/len(whole_features)*100
+
+
+
+
 
     '''
-
-    N = range(1, 10)
-    C = range(30, 5, 50)
-
+                
     ncount = [0]*numclass
-    for index in range(len(testfeatures)):
-        feature = testfeatures[index]
-        name = testnames[index]
+    for index in range(len(test_features)):
+        feature = test_features[index]
+        name = test_names[index]
 
-        priorClusterProb = predictor.calculatePriorProb()
-        classProb = predictor.calculatePosteriorProb(feature, priorClusterProb)
         maxClass = max(classProb, key=classProb.get)
         argsort = np.argsort(classProb.values())
 
         # calculate the accuracy
         ncounter = None
         for ncounter in range(numclass-1, -1, -1):
-            if argsort[ncounter] == testclassid[index]:
+            if argsort[ncounter] == test_classid[index]:
                 break
 
         while ncounter >= 0:
@@ -125,8 +254,8 @@ def main():
             ncounter += -1
 
     # change it to percentage
-    print '# Class: %i \t# Full: %i \t# Partial: %i \t# Test case: %i' % (numclass, numfull, numpartial, numclass*numtest)
-    ncount = [(x * 1.0 / (numtest * numclass)) * 100 for x in ncount]
+    print '# Class: %i \t# Full: %i \t# Partial: %i \t# Test case: %i' % (numclass, numfull, numpartial, numclass*numtestfull)
+    ncount = [(x * 1.0 / (numtestfull * numclass)) * 100 for x in ncount]
     for accindex in range(min(10, len(ncount))):
         print 'N=' + str(accindex+1) + ' C=0 accuracy: ' + str(ncount[accindex])
 
@@ -139,9 +268,11 @@ def main():
     plt.plot([1, len(ncount) + 1], [80, 80], 'g--')
     plt.ylabel('Accuracy %')
     plt.xlabel('N=')
-    plt.title('Sketch: %i # Class: %i   # Full: %i   # Partial: %i # Test case: %i' % (len(train_features), numclass, numfull, numpartial, numclass*numtest))
+    plt.title('Sketch: %i # Class: %i   # Full: %i   # Partial: %i # Test case: %i' % (len(train_features), numclass, numtestfull, numpartial, numclass*numtestfull))
     plt.ylim([0, 105])
     plt.xlim([1, len(ncount)+1])
     plt.grid(True)
     plt.show()
+
+    '''
 if __name__ == "__main__": main()
--- test/featureutil.py
+++ test/featureutil.py
@@ -1,12 +1,48 @@
 import numpy as np
-def partitionfeatures(features, isFull, classId, names, numtestdata, selectTestRandom = True):
+def processName(name):
+    name_split = name.split('_')
+    classname = name_split[0]
+    sketchid = name_split[1]
+    partialid = -1 if bool(len(name_split)==2) else name_split[2]
+
+    return classname, sketchid, partialid
+
+'''
+ Always select training data deterministically,
+ Select test data according to selectTestRandom
+ for easier reproduction of the results
+'''
+
+def partitionfeatures(features, isFull, classId, names, numtrainfull, numtrainpartial, numtestfull, numtestpartial, selectTestRandom = True):
+
     numclass = len(set(classId))
-    testfeatures, testnames, testclassid, testisFull  = [], [], [], []
+    test_features, test_names, test_classId, test_isFull = [], [], [], []
+
+    # Find the
+    sketchPartialMax = dict()
+    for name in names:
+        classname, sketchid, partialid = processName(name)
+        sketchPartialMax[classname + '_' + sketchid] = max(sketchPartialMax[classname + '_' + sketchid], partialid)
+
+    # condition for being training data
+    cond = [bool(processName(names[index])[1] < numtestfull and \
+            processName(names[index])[2] < numtestpartial) for index in range(len(features))]
+
+    test_features.extend([features[index] for index in range(len(features)) if not cond[index]])
+    test_isFull.extend([isFull[index] for index in range(len(isFull)) if not cond[index]])
+    test_names.extend([names[index] for index in range(len(names)) if not cond[index]])
+    test_classId.extend([classId[index] for index in range(len(classId)) if not cond[index]])
+
+    # remove any partial sketch from the traning data
+    train_features = [features[index] for index in range(len(features)) if cond[index]]
+    train_names = [names[index] for index in range(len(names)) if cond[index]]
+    train_isFull = [isFull[index] for index in range(len(isFull)) if cond[index]]
+    train_classId = [classId[index] for index in range(len(classId)) if cond[index]]
 
 
+    return train_features, train_isFull, train_classId, train_names, test_features, test_isFull, test_names, test_classId
 
-
-
+    '''
     for j in range(numclass):
         for i in range(numtestdata):
             # take first partial sketch to be removed
@@ -17,24 +53,22 @@ def partitionfeatures(features, isFull, classId, names, numtestdata, selectTestR
             index = 0
             if selectTestRandom:
                 index = np.random.randint(0, len(features))
-                while testclassid.count(classId[index]) >= numtestdata:
+                while test_classId.count(classId[index]) >= numtestdata:
                     index = np.random.randint(0, len(features))
             else:
-                while testclassid.count(classId[index]) >= numtestdata:
+                while test_classId.count(classId[index]) >= numtestdata:
                     index += 1
 
-            fullindex = names[index].split('_')[0] + '_' + names[index].split('_')[1]
-            classname = names[index].split('_')[0]
-            sketchid = names[index].split('_')[1]
+            classname, sketchid = processName(names[index])
 
             # get test data mask
             cond = [names[partialindex].split('_')[0] == classname and names[partialindex].split('_')[1] == sketchid for partialindex in range(len(features))]
 
             # get only the full sketches for test data
-            testfeatures.extend([features[index] for index in range(len(features)) if cond[index]])
-            testisFull.extend([isFull[index] for index in range(len(isFull)) if cond[index]])
-            testnames.extend([names[index] for index in range(len(names)) if cond[index]])
-            testclassid.extend([classId[index] for index in range(len(classId)) if cond[index]])
+            test_features.extend([features[index] for index in range(len(features)) if cond[index]])
+            test_isFull.extend([isFull[index] for index in range(len(isFull)) if cond[index]])
+            test_names.extend([names[index] for index in range(len(names)) if cond[index]])
+            test_classId.extend([classId[index] for index in range(len(classId)) if cond[index]])
 
             # remove any partial sketch from the traning data
             features = [features[index] for index in range(len(features)) if not cond[index]]
@@ -42,40 +76,103 @@ def partitionfeatures(features, isFull, classId, names, numtestdata, selectTestR
             isFull = [isFull[index] for index in range(len(isFull)) if not cond[index]]
             classId = [classId[index] for index in range(len(classId)) if not cond[index]]
 
-    return train_features, train_isFull, train_classId, train_names, test_features, test_isFull, test_names, test_classid
+    return train_features, train_isFull, train_classId, train_names, test_features, test_isFull, test_names, test_classId
+    '''
 
-def partitionfeatures_notin(features, isFull, classId, names, numtestdata, trainnames, selectTestRandom = True):
+def partitionfeatures_notin(features, isFull, classId, names, numtestfull, numtestpartial, trainnames, selectTestRandom = True):
     numclass = len(set(classId))
 
-    testfeatures = [features[index] for index in range(len(features)) if names[index] not in trainnames]
-    testisFull = [isFull[index] for index in range(len(isFull)) if names[index] not in trainnames]
-    testclassid = [classId[index] for index in range(len(classId)) if names[index] not in trainnames]
-    testnames = [names[index] for index in range(len(names)) if names[index] not in trainnames]
+    test_features = [features[index] for index in range(len(features)) if names[index] not in trainnames]
+    test_isFull = [isFull[index] for index in range(len(isFull)) if names[index] not in trainnames]
+    test_classId = [classId[index] for index in range(len(classId)) if names[index] not in trainnames]
+    test_names = [names[index] for index in range(len(names)) if names[index] not in trainnames]
 
-    features = [features[index] for index in range(len(features)) if names[index] in trainnames]
-    isFull = [isFull[index] for index in range(len(isFull)) if names[index] in trainnames]
-    classId = [classId[index] for index in range(len(classId)) if names[index] in trainnames]
-    names = [names[index] for index in range(len(names)) if names[index] in trainnames]
+    train_features = [features[index] for index in range(len(features)) if names[index] in trainnames]
+    train_isFull = [isFull[index] for index in range(len(isFull)) if names[index] in trainnames]
+    train_classId = [classId[index] for index in range(len(classId)) if names[index] in trainnames]
+    train_names = [names[index] for index in range(len(names)) if names[index] in trainnames]
 
-    if len(testfeatures) < numtestdata:
+    '''
+    Divided data into train and test
+    But now we need to get numtestfull full sketch and
+    numtestpartial partial sketch
+    '''
+
+    test_full_features = [test_features[index] for index in range(len(test_features)) if test_isFull[index]]
+    test_full_classId = [test_classId[index] for index in range(len(test_classId)) if test_isFull[index]]
+    test_full_names = [test_names[index] for index in range(len(test_names))  if test_isFull[index]]
+    test_full_isFull = [test_isFull[index] for index in range(len(test_isFull)) if test_isFull[index]]
+
+    test_partial_features = [test_features[index] for index in range(len(test_features)) if not test_isFull[index]]
+    test_partial_classId = [test_classId[index] for index in range(len(test_classId)) if not test_isFull[index]]
+    test_partial_names = [test_names[index] for index in range(len(test_names))  if not test_isFull[index]]
+    test_partial_isFull = [test_isFull[index] for index in range(len(test_isFull)) if not test_isFull[index]]
+
+    numtestfull = min(numtestfull, len(test_full_features))
+    numtestpartial = min(numtestpartial, len(test_partial_features))
+
+    if selectTestRandom:
+        import random
+        test_full_features = random.sample(test_full_features, numtestfull)
+        test_full_isFull = random.sample(test_full_isFull, numtestfull)
+        test_full_classId = random.sample(test_full_classId, numtestfull)
+        test_full_names = random.sample(test_full_names, numtestfull)
+
+        test_partial_features = random.sample(test_partial_features, numtestpartial)
+        test_partial_isFull = random.sample(test_partial_isFull, numtestpartial)
+        test_partial_classId = random.sample(test_partial_classId, numtestpartial)
+        test_partial_names = random.sample(test_partial_names, numtestpartial)
+    else:
+        test_full_features = test_full_features[:numtestfull]
+        test_full_isFull = test_full_isFull[:numtestfull]
+        test_full_classId = test_full_classId[:numtestfull]
+        test_full_names = test_full_names[:numtestfull]
+
+        test_partial_features = test_partial_features[:numtestpartial]
+        test_partial_isFull = test_partial_isFull[:numtestpartial]
+        test_partial_classId = test_partial_classId[:numtestpartial]
+        test_partial_names = test_partial_names[:numtestpartial]
+
+    '''
+    if len(test_features) < numtestdata:
         raise Warning('not enough test data')
 
     if selectTestRandom:
         import random
-        testfeatures = random.sample(testfeatures, numtestdata)
-        testisFull = random.sample(testisFull, numtestdata)
-        testclassid = random.sample(testclassid, numtestdata)
-        testnames = random.sample(testnames, numtestdata)
+        test_features = random.sample(test_features, numtestdata)
+        test_isFull = random.sample(test_isFull, numtestdata)
+        test_classId = random.sample(test_classId, numtestdata)
+        test_names = random.sample(test_names, numtestdata)
     else:
-        testfeatures = testfeatures[:numtestdata]
-        testisFull = testisFull[:numtestdata]
-        testclassid = testclassid[:numtestdata]
-        testnames = testnames[:numtestdata]
+        test_features = test_features[:numtestdata]
+        test_isFull = test_isFull[:numtestdata]
+        test_classId = test_classId[:numtestdata]
+        test_names = test_names[:numtestdata]
+    '''
 
-    return features, isFull, classId, names, testfeatures, testisFull, testnames, testclassid
+    test_features = test_full_features + test_partial_features
+    test_isFull = test_full_isFull + test_partial_isFull
+    test_classId = test_full_classId + test_partial_classId
+    test_names = test_full_names + test_partial_names
+
+    return train_features, train_isFull, train_classId, train_names, test_features, test_isFull, test_names, test_classId
 
 
-    def processName(name):
-        classname = names[index].split('_')[0]
-        sketchid = names[index].split('_')[1]
-        return
+def xfoldpartition(
+    xfold,
+    whole_features,
+    whole_isFull,
+    whole_classId,
+    whole_names,
+    folderList):
+
+    xfold_features, xfold_names, xfold_classId, xfold_isFull = [], [], [], []
+
+    for iter in range(xfold):
+        cond = [bool(int(processName(whole_names[index])[1]) < (iter+1)*80/xfold) for index in range(len(whole_features))]
+        xfold_features.append([whole_features[index] for index in range(len(whole_features)) if cond[index]])
+        xfold_isFull.append([whole_isFull[index] for index in range(len(whole_isFull)) if cond[index]])
+        xfold_classId.append([whole_classId[index] for index in range(len(whole_classId)) if cond[index]])
+        xfold_names.append([whole_names[index] for index in range(len(whole_names)) if cond[index]])
+
+    return xfold_features, xfold_isFull, xfold_classId, xfold_names
